37325a0388ed2325103e1fc83cfc418df13d986b
Lilith Naomi Nightingale <yretenai@gmail.com>
2021-08-18 18:12:18 +0100

    Add project files

---
+++ SteamFiles.Processor/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SteamFiles.Processor
+{
+    public class Class1
+    {
+    }
+}
---
+++ SteamFiles/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SteamFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
